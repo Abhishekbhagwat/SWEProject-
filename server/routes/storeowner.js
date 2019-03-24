39aa24171498fff1dd0eaa940db52a10683fe6ae
YingTing04 <yingting2404@gmail.com>
2019-03-24 18:07:35 +0800

    Added storeowner routes

--- server/routes/storeowner.js
+++ server/routes/storeowner.js
@@ -7,7 +7,61 @@ module.exports = (Dish, Location, Order, Store, User) => {
     else next('not store owner');
   });
 
+  //add a dish
+  router.post('/addDish', (req, res) => {
+    Promise.all(req.body.dishes.map((dishId) => Dish.findById(dishId)))
+    .then((dish) => {
+      (new Dish({
+        name: req.body.name,
+        store: req.body.store,
+        price: req.body.price,
+      })).save()
+      .then(() => res.json({success: true}))
+      .catch(next);
+    }).catch(() => next('dishes addition unsuccessful'));
+  });
 
+  //update dish name
+  router.post('/updateDishName/:name', (req, res) => {
+    Store.findByIdAndUpdate(req.body.dish, {$push: {dish.name: req.params.name}})
+    .then((stores) => res.json({success: true, stores: stores}))
+    .catch(next);
+  })
+
+  //update dish description
+  router.post('/updateDishPrice/:description', (req, res) => {
+    Store.findByIdAndUpdate(req.body.dish, {$push: {dish.description: req.params.description}})
+    .then((stores) => res.json({success: true, stores: stores}))
+    .catch(next);
+  })
+
+  //update dish price
+  router.post('/updateDishPrice/:price', (req, res) => {
+    Store.findByIdAndUpdate(req.body.dish, {$push: {dish.price: req.params.price}})
+    .then((stores) => res.json({success: true, stores: stores}))
+    .catch(next);
+  })
+
+//remove a dish
+router.post('/removeDish', (req, res) => {
+   Dish.remove({_id: req.body.dish})
+   .then(() => res.json({success: true}))
+   .catch(() => res.json({success: false}));
+  });
+
+  //get all orders by store
+  router.get('/storeOrders', (req, res) => {
+    Promise.all(req.store.orders.map((order) => Order.findById(order)))
+    .then((orders) => res.json({success: true, orders: orders}))
+    .catch(next);
+  });
+
+  //update order completion status
+  router.post('/updateCompletionStatus', (req, res) => {
+    Store.findByIdAndUpdate(req.body.orders, {$push: {order.completed: true}})
+    .then((stores) => res.json({success: true, stores: stores}))
+    .catch(next);
+  })
 
   return router;
 }
